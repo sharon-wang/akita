9ccdb2b6001b66b9aa49dc1a3cd0342b34b23a95
sharon-wang <sharon-wang-cpsc@outlook.com>
2020-05-24 13:32:17 -0400

    Test browser notifications
    Signed-off-by: sharon-wang <sharon-wang-cpsc@outlook.com>

--- popup.js
+++ popup.js
@@ -1,2 +1,19 @@
 //test code
-browser.notifications.create('hello!');
+document.getElementById("test-notification-button").addEventListener("click", function testNotification() {
+	const testNotificationId = "test-notification";
+	let webBrowser = chrome ? chrome : browser;
+
+	webBrowser.notifications.create(testNotificationId, {
+		"type": "basic",
+		"iconUrl": "icon.png",
+		"title": "just testing notifications",
+		"message": "hello hello is anyone out there?"
+	});
+
+	webBrowser.browserAction.onClicked.addListener(()=> {
+		var clearing = webBrowser.notifications.clear(testNotificationId);
+		clearing.then(() => {
+		console.log("cleared");
+		});
+	});
+});
